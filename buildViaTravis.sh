3e968f53c13239972622e8895d0c556e920293cd
chrnie <chrnie@qq.com>
2019-03-03 22:32:41 +0800

    Add ci config

---
+++ buildViaTravis.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Auto build or deploy
+
+set -e
+
+if [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_TAG" != "" ]; then
+  echo "Build for Release => Branch ["$TRAVIS_BRANCH"]  Tag ["$TRAVIS_TAG"]"
+  ./gradlew -PbintrayUser="$BINTRAY_USER" -PbintrayKey="$BINTRAY_KEY" clean bintrayUpload
+else
+  echo "Build => Branch ["$TRAVIS_BRANCH"]  Tag ["$TRAVIS_TAG"]"
+  ./gradlew build
+fi
