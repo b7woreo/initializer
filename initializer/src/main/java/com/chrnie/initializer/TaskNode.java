7534a02ac04ec7f8d4cc36b166d8a91e2200df5d
chrnie <chrnie@qq.com>
2019-03-10 22:44:35 +0800

    Add log message in core

--- initializer/src/main/java/com/chrnie/initializer/TaskNode.java
+++ initializer/src/main/java/com/chrnie/initializer/TaskNode.java
@@ -1,13 +1,18 @@
 package com.chrnie.initializer;
 
 import android.content.Context;
+import android.util.Log;
 import com.chrnie.initializer.exception.CyclicDependencyException;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Locale;
 import java.util.Set;
 import java.util.concurrent.Executor;
 
 final class TaskNode extends GraphNode<TaskNode> {
+
+  private static final String ROOT_NAME = "Root";
+  private static final String TAG = "Initializer";
 
   private final String name;
   private final Executor executor;
@@ -20,7 +25,7 @@ final class TaskNode extends GraphNode<TaskNode> {
   }
 
   TaskNode(String name, Executor executor, Action action) {
-    this.name = name;
+    this.name = name != null ? name : ROOT_NAME;
     this.executor = executor != null ? executor : MainExecutor.get();
     this.action = action != null ? action : Action.EMPTY;
   }
@@ -42,7 +47,15 @@ final class TaskNode extends GraphNode<TaskNode> {
     final Runnable r = new Runnable() {
       @Override
       public void run() {
+        long startTime = System.currentTimeMillis();
+
+        Log.d(TAG, String.format(Locale.ENGLISH,
+            "Start task: [%s : %s]", name, Thread.currentThread().getName()
+        ));
         action.call(context);
+        Log.d(TAG, String.format(Locale.ENGLISH,
+            "End task: [%s : %d]", name, (System.currentTimeMillis() - startTime))
+        );
 
         for (TaskNode child : TaskNode.this.getChildren()) {
           child.executeInternal(context, TaskNode.this);
